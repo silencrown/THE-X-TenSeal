a907e9aff7776d20ec1fc75bac99fca838ef6083
Shiqi Gao <shiqiG@outlook.com>
2023-04-12 14:11:32 +0000

    experiment of linear size test

--- test/test_linear.py
+++ test/test_linear.py
@@ -16,28 +16,34 @@ class TestLinear(unittest.TestCase):
     def setUp(self):
         self.context_manager = ContextManager()
 
-    def test_Linear(self):
+    def test_Linear_size(self):
+        for num_size in range(1, 100):
+            self._test_Linear(num_size)
+            logger(f"input tensor size: {num_size} passed")
+
+    def _test_Linear(self, num_size):
+        logger.log_system_info()
         # create a random input tensor
-        tensor = torch.randn(1, 8, requires_grad=False)
+        tensor = torch.randn(1, num_size, requires_grad=False)
         enc_tensor = ts.ckks_tensor(self.context_manager.context, tensor.tolist())
         # logger(f"tensor: {tensor[0]}")
 
         # torch model inference
-        torch_model = Linear(8, 8)
+        torch_model = Linear(num_size, num_size)
         result = torch_model(tensor).tolist()
-        logger(f"result: {result}")
+        logger(f"result: {result[0][0]}")
 
         # enc model inference
         enc_model = EncLinear(torch_model)
+        logger.log_system_info()
         enc_result = np.array(enc_model(enc_tensor).decrypt().tolist())
-        logger(f"enc_result: {enc_result}")
-        # logger.log_system_info()
+        logger(f"enc_result: {enc_result[0][0]}")
+
         
         # decrypt and check the values
         np.testing.assert_array_almost_equal(enc_result, 
                                              result, 
                                              decimal=4)
-
 
 if __name__ == '__main__':
     unittest.main()
